ffc1f2becd5dc6df262e1104611cef9461572361
ccandy <ccunandy2001@gmail.com>
2025-05-10 12:20:48 +0800

    Update GameManager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
+        EventManager.Instance.TriggerEvent("OnSceneLoaded", sceneName);
     }
+    
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        EventManager.Instance.TriggerEvent("OnGameQuit");
+    }
+    
 }
