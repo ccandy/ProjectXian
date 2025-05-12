ef6471a4e7d69f1a1934023b23d2b6b8ce21375a
ccandy <ccunandy2001@gmail.com>
2025-05-12 16:19:54 +0800

    Update AffectionManager.cs

--- Assets/Scripts/Manager/AffectionManager.cs
+++ Assets/Scripts/Manager/AffectionManager.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AffectionManager : MonoBehaviour
+/// <summary>
+/// 管理角色的好感度点数，值在 minAffection 到 maxAffection 范围内。
+/// 当好感度变化时触发事件通知。
+/// </summary>
+public class AffectionManager : SingletonMonoBehaviour<AffectionManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private Dictionary<string ,int> affectionDictionary = new Dictionary<string ,int>();
+   private const int minAffection = 0;
+   private const int maxAffection = 100;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void ModifyAffection(string affectionKey, int affectionValue)
+   {
+      if (!affectionDictionary.ContainsKey(affectionKey))
+      {
+         affectionDictionary[affectionKey] = 0;
+      }
+
+      int increaseValue = affectionDictionary[affectionKey] + affectionValue;
+      int newValue = Mathf.Clamp(increaseValue, minAffection, maxAffection);
+      AffectionEventData data = new AffectionEventData(affectionKey, newValue);
+      EventManager.Instance.TriggerEvent("OnAffectionChanged", data);
+   }
+
+   public int GetAffectionValue(string affectionKey)
+   {
+      return affectionDictionary.ContainsKey(affectionKey) ? affectionDictionary[affectionKey] : 0;
+   }
+   
+   public Dictionary<string,int> GetAllAffection() => new Dictionary<string,int>(affectionDictionary);
+
+   public void SetAffection(string characterName, int value)
+   {
+      affectionDictionary[characterName] = Mathf.Clamp(value, minAffection, maxAffection);
+   }
+   
+}
+
+public struct AffectionEventData
+{
+   public string CharacterName;
+   public int NewValue;
+
+   public AffectionEventData(string characterName, int newValue)
+   {
+      CharacterName = characterName;
+      NewValue = newValue;
+   }
 }
